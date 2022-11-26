ccb855d95b6cc7200517a747345cdf7694d879cd
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2022-11-25 23:46:06 -0300

    conexion a la base de datos termianda?

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+    include("src/services/dbconnection.php");
+
+    $con = conectar();
+
+    echo 'Conectado correctamente';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,11 +14,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/css/styles.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <title>Iniciar sesión</title>
 </head>
 
 <body>
-    <h1>login</h1>
+    <div class="login">
+        <h1>Login</h1>
+        <form action="authenticate.php" method="post">
+            <label for="username">
+                <i class="fas fa-user"></i>
+            </label>
+            <input type="text" name="username" placeholder="Username" id="username" required>
+            <label for="password">
+                <i class="fas fa-lock"></i>
+            </label>
+            <input type="password" name="password" placeholder="Password" id="password" required>
+            <input type="submit" value="Login">
+        </form>
+    </div>
 </body>
 
 </html>
--- src/services/dbconnection.php
+++ src/services/dbconnection.php
@@ -1,6 +1,12 @@
-<?php 
-$user = "root";
-$password = "";
-$server = "localhost";
-$db_name = "";
-/* $conn = mysqli_connect($server, $user, $pass) or die("Error al conectar".mysqli_errno()); */
+<?php
+function conectar()
+{
+    $user = "root";
+    $password = "";
+    $server = "localhost";
+    $db_name = "proyectoagustinlautaro";
+    $conn = mysqli_connect($server, $user, $password) or die("Error al conectar");
+    mysqli_connect($server, $user, $password, $db_name);
+
+    return $conn;
+}
