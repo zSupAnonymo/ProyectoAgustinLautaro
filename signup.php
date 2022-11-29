d192c4382f886e868bf32289ddf0d99658ccc6bb
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2022-11-29 01:50:44 -0300

    modificacion database connection

--- signup.php
+++ signup.php
@@ -1,5 +1,5 @@
 <?php
-    require 'database.php';
+    require 'database_connect.php';
 
     $message = '';
 
@@ -26,7 +26,7 @@
         <!-- BOOTSTRAP -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <!-- CSS -->
-        <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="styles/register.css">
 	</head>
 	<body>
 
