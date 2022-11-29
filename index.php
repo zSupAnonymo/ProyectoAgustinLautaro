ce5c5ad21396a3c404c1398f0ceb577add72535c
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2022-11-29 12:19:41 -0300

    aplicacion css index.php

--- index.php
+++ index.php
@@ -21,21 +21,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="styles/index.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
 
 <?php if(!empty($user)): ?>
-      <br> Welcome. <?= $user['email']; ?>
-      <br>You are Successfully Logged In
+      <br> Bienvenido, <?= $user['email']; ?>
+      <br>Has iniciado sesión correctamente
       <a href="logout.php">
-        Logout
+        Cerrar Sesión
       </a>
     <?php else: ?>
-      <h1>Please Login or SignUp</h1>
+    <section class="welcome d-flex flex-column justify-content-center">
+      <div class="box-welcome container-fluid text-center">
+        <h1 class="fs-2 fw-bold">Por favor inicia sesión o regístrate.</h1>
 
-      <a href="login.php">Login</a> or
-      <a href="signup.php">SignUp</a>
+        <div class="btn-grid d-grid gap-2">
+          <a href="login.php" class="btn-login btn btn-primary py-3 fs-4" role="button">Iniciar Sesión</a>
+          <a href="signup.php" class="btn-signup btn btn-primary py-3 fs-4" role="button">Registrarse</a>
+        </div>
+
+      </div>
+    </section>
     <?php endif; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </body>
 </html>
